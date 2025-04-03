b78240709e9a71690e806b8e3d7789de44704b52
Jericho Earl <jerichomartin805@gmail.com>
2025-04-03 09:45:51 +0800

    2025 Update

---
+++ Library Management System/Usercontrol/StaffUserControl/FindMemberUserControl.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Library_Management_System.Usercontrol.StaffUserControl
+{
+    partial class FindMemberUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelMembers = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearchMember = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAddMember = new System.Windows.Forms.Button();
+            this.btnDeleteMember = new System.Windows.Forms.Button();
+            this.btnEditMember = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelMembers
+            // 
+            this.flowLayoutPanelMembers.AutoScroll = true;
+            this.flowLayoutPanelMembers.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanelMembers.Location = new System.Drawing.Point(0, 149);
+            this.flowLayoutPanelMembers.Name = "flowLayoutPanelMembers";
+            this.flowLayoutPanelMembers.Size = new System.Drawing.Size(1006, 463);
+            this.flowLayoutPanelMembers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search For Member";
+            // 
+            // txtSearchMember
+            // 
+            this.txtSearchMember.Location = new System.Drawing.Point(381, 53);
+            this.txtSearchMember.Name = "txtSearchMember";
+            this.txtSearchMember.Size = new System.Drawing.Size(210, 29);
+            this.txtSearchMember.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(597, 55);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(81, 27);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnAddMember
+            // 
+            this.btnAddMember.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAddMember.FlatAppearance.BorderSize = 0;
+            this.btnAddMember.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddMember.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMember.Location = new System.Drawing.Point(384, 98);
+            this.btnAddMember.Name = "btnAddMember";
+            this.btnAddMember.Size = new System.Drawing.Size(94, 20);
+            this.btnAddMember.TabIndex = 26;
+            this.btnAddMember.Text = "Add";
+            this.btnAddMember.UseVisualStyleBackColor = false;
+            this.btnAddMember.Visible = false;
+            this.btnAddMember.Click += new System.EventHandler(this.btnAddMember_Click);
+            // 
+            // btnDeleteMember
+            // 
+            this.btnDeleteMember.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDeleteMember.FlatAppearance.BorderSize = 0;
+            this.btnDeleteMember.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeleteMember.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteMember.Location = new System.Drawing.Point(584, 98);
+            this.btnDeleteMember.Name = "btnDeleteMember";
+            this.btnDeleteMember.Size = new System.Drawing.Size(94, 20);
+            this.btnDeleteMember.TabIndex = 27;
+            this.btnDeleteMember.Text = "Delete";
+            this.btnDeleteMember.UseVisualStyleBackColor = false;
+            this.btnDeleteMember.Visible = false;
+            this.btnDeleteMember.Click += new System.EventHandler(this.btnDeleteMember_Click);
+            // 
+            // btnEditMember
+            // 
+            this.btnEditMember.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEditMember.FlatAppearance.BorderSize = 0;
+            this.btnEditMember.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditMember.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditMember.Location = new System.Drawing.Point(484, 98);
+            this.btnEditMember.Name = "btnEditMember";
+            this.btnEditMember.Size = new System.Drawing.Size(94, 20);
+            this.btnEditMember.TabIndex = 28;
+            this.btnEditMember.Text = "Edit";
+            this.btnEditMember.UseVisualStyleBackColor = false;
+            this.btnEditMember.Visible = false;
+            this.btnEditMember.Click += new System.EventHandler(this.btnEditMember_Click);
+            // 
+            // FindMemberUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAddMember);
+            this.Controls.Add(this.btnDeleteMember);
+            this.Controls.Add(this.btnEditMember);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchMember);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanelMembers);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FindMemberUserControl";
+            this.Size = new System.Drawing.Size(1006, 612);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMembers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearchMember;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAddMember;
+        private System.Windows.Forms.Button btnDeleteMember;
+        private System.Windows.Forms.Button btnEditMember;
+    }
+}
---
+++ Library Management System/Usercontrol/StaffUserControl/FindMemberUserControl.cs
@@ -0,0 +1,235 @@
+﻿using Library_Management_System.Class;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System.Usercontrol.StaffUserControl
+{
+    public partial class FindMemberUserControl : UserControl
+    {
+        private readonly string connectionString = "Server=localhost;Database=librarydb;Uid=root;Pwd=martinjericho22@2002;";
+        public FindMemberUserControl()
+        {
+            InitializeComponent();
+            LoadAllMembers(); // Load all members on control load
+        }
+        private void LoadAllMembers()
+        {
+            SearchStudent(""); // Pass empty string to load all members
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string studentName = txtSearchMember.Text.Trim();
+
+            if (!string.IsNullOrEmpty(studentName))
+            {
+                SearchStudent(studentName);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a student name to search.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void SearchStudent(string studentName)
+        {
+            string query = @"
+    SELECT 
+        m.Member_Id AS 'Student ID',
+        m.first_name AS 'First Name',
+        m.last_name AS 'Last Name',
+        m.Email AS 'Email',
+        m.Phone AS 'Phone',
+        m.profile_pic AS 'Profile Pic'
+    FROM 
+        Members_tbl m
+    WHERE 
+        @Name = '' OR m.last_name LIKE @Name OR CONCAT(m.first_name, ' ', m.last_name) LIKE @Name
+    ORDER BY 
+        m.Member_Id";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Name", "%" + studentName + "%");
+                        MySqlDataReader reader = cmd.ExecuteReader();
+
+                        flowLayoutPanelMembers.Controls.Clear(); // Clear existing controls
+
+                        while (reader.Read())
+                        {
+                            // Create a panel for each member
+                            int memberId = Convert.ToInt32(reader["Student ID"]);
+                            string fullName = $"{reader["First Name"]} {reader["Last Name"]}";
+                            string email = reader["Email"].ToString();
+                            string phone = reader["Phone"].ToString();
+                            string profilePic = reader["Profile Pic"].ToString();
+
+                            Panel memberPanel = CreateMemberPanel(memberId, fullName, email, phone, profilePic);
+                            flowLayoutPanelMembers.Controls.Add(memberPanel);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnAddMember_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Database=librarydb;Uid=root;Pwd=martinjericho22@2002;";
+            MemberRepository memberRepository = new MemberRepository(connectionString);
+            FormAddMember formAddMember = new FormAddMember(memberRepository);
+            formAddMember.ShowDialog();
+        }
+
+        private void btnEditMember_Click(object sender, EventArgs e)
+        {
+            Button btnEdit = sender as Button;
+            int memberId = (int)btnEdit.Tag;
+
+            FormEditMember formEditMember = new FormEditMember(memberId);
+            formEditMember.ShowDialog();
+
+            // Refresh the FlowLayoutPanel after editing
+            SearchStudent("");
+        }
+
+        private void btnDeleteMember_Click(object sender, EventArgs e)
+        {
+            Button btnDelete = sender as Button;
+            int memberId = (int)btnDelete.Tag;
+
+            DialogResult result = MessageBox.Show("Are you sure you want to delete this member?",
+                                                  "Confirm Delete",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                DeleteMemberFromDatabase(memberId);
+                SearchStudent(""); // Refresh FlowLayoutPanel
+            }
+        }
+        private void DeleteMemberFromDatabase(int memberId)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "DELETE FROM Members_tbl WHERE Member_Id = @MemberId";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@MemberId", memberId);
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Member deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to delete the member. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting member: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private Panel CreateMemberPanel(int memberId, string fullName, string email, string phone, string profilePic)
+        {
+            Panel panel = new Panel
+            {
+                Size = new Size(350, 120),
+                BorderStyle = BorderStyle.FixedSingle,
+                Padding = new Padding(5)
+            };
+
+            // Profile Picture
+            PictureBox pictureBox = new PictureBox
+            {
+                Size = new Size(80, 80),
+                Location = new Point(10, 10),
+                BorderStyle = BorderStyle.FixedSingle,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+
+            if (!string.IsNullOrEmpty(profilePic) && System.IO.File.Exists(profilePic))
+            {
+                pictureBox.Image = Image.FromFile(profilePic);
+            }
+
+            panel.Controls.Add(pictureBox);
+
+            // Member Details
+            Label lblName = new Label
+            {
+                Text = $"Name: {fullName}",
+                AutoSize = true,
+                Location = new Point(100, 10)
+            };
+            panel.Controls.Add(lblName);
+
+            Label lblEmail = new Label
+            {
+                Text = $"Email: {email}",
+                AutoSize = true,
+                Location = new Point(100, 35)
+            };
+            panel.Controls.Add(lblEmail);
+
+            Label lblPhone = new Label
+            {
+                Text = $"Phone: {phone}",
+                AutoSize = true,
+                Location = new Point(100, 60)
+            };
+            panel.Controls.Add(lblPhone);
+
+            // Edit Button
+            Button btnEdit = new Button
+            {
+                Text = "Edit",
+                Size = new Size(80, 30),
+                Location = new Point(190, 85),
+                BackColor = Color.Green,
+                Tag = memberId // Store memberId in Tag for reference
+            };
+            btnEdit.Click += btnEditMember_Click;
+            panel.Controls.Add(btnEdit);
+
+            // Delete Button
+            Button btnDelete = new Button
+            {
+                Text = "Delete",
+                Size = new Size(80, 30),
+                Location = new Point(270, 85),
+                BackColor = Color.Red,
+                Tag = memberId // Store memberId in Tag for reference
+            };
+            btnDelete.Click += btnDeleteMember_Click;
+            panel.Controls.Add(btnDelete);
+
+            return panel;
+        }
+    }
+}
